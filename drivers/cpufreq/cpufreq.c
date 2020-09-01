b8bd17d5d276d5c7ad6f6d9b7b3619e0c2854778
Ionela Voinescu <ionela.voinescu@arm.com>
2020-09-01 21:55:47 +0100

    cpufreq: report whether cpufreq supports Frequency Invariance (FI)
    Now that the update of the FI scale factor is done in cpufreq core for
    selected functions - target(), target_index() and fast_switch(),
    we can provide feedback to the task scheduler and architecture code
    on whether cpufreq supports FI.
    
    For this purpose provide an external function to expose whether the
    cpufreq drivers support FI, by using a static key.
    
    The logic behind the enablement of cpufreq-based invariance is as
    follows:
     - cpufreq-based invariance is disabled by default
     - cpufreq-based invariance is enabled if any of the callbacks
       above is implemented while the unsupported setpolicy() is not
    
    The cpufreq_supports_freq_invariance() function only returns whether
    cpufreq is instrumented with the arch_set_freq_scale() calls that
    result in support for frequency invariance. Due to the lack of knowledge
    on whether the implementation of arch_set_freq_scale() actually results
    in the setting of a scale factor based on cpufreq information, it is up
    to the architecture code to ensure the setting and provision of the
    scale factor to the scheduler.
    
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: aizen <275144853+sillyaizen@users.noreply.github.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2806,6 +2806,15 @@ int cpufreq_register_driver(struct cpufreq_driver *driver_data)
 	cpufreq_driver = driver_data;
 	write_unlock_irqrestore(&cpufreq_driver_lock, flags);
 
+	/*
+	 * Mark support for the scheduler's frequency invariance engine for
+	 * drivers that implement target(), target_index() or fast_switch().
+	 */
+	if (!cpufreq_driver->setpolicy) {
+		static_branch_enable_cpuslocked(&cpufreq_freq_invariance);
+		pr_debug("supports frequency invariance");
+	}
+
 	if (driver_data->setpolicy)
 		driver_data->flags |= CPUFREQ_CONST_LOOPS;
 
