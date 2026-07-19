44d19cbf6876da7f6da8ebabc4398de840ad9782
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2026-07-19 10:53:41 -0400

    usb: gadget: function: Simplify diagnostic messaging in printer
    [ Upstream commit 57b7b733b1a7aeab25bc2670afff608214284863 ]
    
    Don't issue messages which can be easily achieved with ftrace.
    In case of printer_open() the return code is propagated to other layers
    so the user will know about -EBUSY anyway.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
    Link: https://lore.kernel.org/r/20221123110746.59611-1-andrzej.p@collabora.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 30adce93d5c4 ("usb: gadget: f_printer: take kref only for successful open")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/f_printer.c
+++ drivers/usb/gadget/function/f_printer.c
@@ -365,7 +365,7 @@ printer_open(struct inode *inode, struct file *fd)
 	spin_unlock_irqrestore(&dev->lock, flags);
 
 	kref_get(&dev->kref);
-	DBG(dev, "printer_open returned %x\n", ret);
+
 	return ret;
 }
 
@@ -383,7 +383,6 @@ printer_close(struct inode *inode, struct file *fd)
 	spin_unlock_irqrestore(&dev->lock, flags);
 
 	kref_put(&dev->kref, printer_dev_free);
-	DBG(dev, "printer_close\n");
 
 	return 0;
 }
@@ -869,8 +868,6 @@ static void printer_reset_interface(struct printer_dev *dev)
 	if (dev->interface < 0)
 		return;
 
-	DBG(dev, "%s\n", __func__);
-
 	if (dev->in_ep->desc)
 		usb_ep_disable(dev->in_ep);
 
@@ -907,8 +904,6 @@ static int set_interface(struct printer_dev *dev, unsigned number)
 static void printer_soft_reset(struct printer_dev *dev)
 {
 	struct usb_request	*req;
-
-	INFO(dev, "Received Printer Reset Request\n");
 
 	if (usb_ep_disable(dev->in_ep))
 		DBG(dev, "Failed to disable USB in_ep\n");
@@ -1205,8 +1200,6 @@ static int printer_func_set_alt(struct usb_function *f,
 static void printer_func_disable(struct usb_function *f)
 {
 	struct printer_dev *dev = func_to_printer(f);
-
-	DBG(dev, "%s\n", __func__);
 
 	printer_reset_interface(dev);
 }
