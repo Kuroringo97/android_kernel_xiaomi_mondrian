46483cd3157e8d88b4b45f479195bb55009fd01a
EmanuelCN <emanuelghub@gmail.com>
2026-09-19 23:50:36 +0000

    block/ssg: Tune default values
    Reduce read request expiration from 500ms to 48ms and write
    expiration from 5s to 1s to reduce latency.
    
    Disable front merges to avoid unnecessary merge lookups for typical
    UFS workloads.
    
    Signed-off-by: EmanuelCN <emanuelghub@gmail.com>

--- block/ssg-iosched.c
+++ block/ssg-iosched.c
@@ -41,8 +41,8 @@ extern void blk_sec_stats_account_io_done(
 
 #define MAX_ASYNC_WRITE_RQS	8
 
-static const int read_expire = HZ / 10;		/* max time before a read is submitted. */
-static const int write_expire = HZ / 2;		/* ditto for writes, these limits are SOFT! */
+static const int read_expire = HZ / 20;		/* max time before a read is submitted. */
+static const int write_expire = HZ;		/* ditto for writes, these limits are SOFT! */
 static const int max_write_starvation = 2;	/* max times reads can starve a write */
 static const int max_async_write_ratio = 25;	/* maximum service ratio for async write */
 
@@ -580,10 +580,7 @@ static int ssg_init_queue(struct request_queue *q, struct elevator_type *e)
 	ssg->fifo_expire[READ]  = read_expire;
 	ssg->fifo_expire[WRITE] = write_expire;
 	ssg->max_write_starvation = max_write_starvation;
-	ssg->front_merges = 1;
-	blk_queue_max_hw_sectors(q, 128 * 8);
-	q->backing_dev_info->ra_pages = 128;
-	blk_queue_rq_timeout(q, 5000);
+	ssg->front_merges = 0;
 
 	atomic_set(&ssg->allocated_rqs, 0);
 	atomic_set(&ssg->async_write_rqs, 0);
